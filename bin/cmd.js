a3d3f32c727d5412f562a704212e9bd23bfdcfe1
Maximilian Antoni <mail@maxantoni.de>
2014-01-23 22:53:51 +0100

    Defaults

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,6 +5,22 @@
 var driver  = require('../lib/driver');
 var fs      = require('fs');
 
-var options = JSON.parse(fs.readFileSync('.min-wd').toString());
+var options = {
+  hostname : 'localhost',
+  port     : 4444,
+  browsers : [{
+    name   : 'chrome'
+  }]
+};
+
+if (fs.existsSync('.min-wd')) {
+  var overrides = JSON.parse(fs.readFileSync('.min-wd').toString());
+  var k;
+  for (k in overrides) {
+    if (overrides.hasOwnProperty(k)) {
+      options[k] = overrides[k];
+    }
+  }
+}
 
 driver(process.stdin, options).pipe(process.stdout);
