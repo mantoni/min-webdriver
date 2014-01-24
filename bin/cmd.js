145cab3ea9eab2f50849edd8f180a924d30a036d
Maximilian Antoni <mail@maxantoni.de>
2014-01-24 13:42:22 +0100

    Move options into separate file

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,26 +1,7 @@
 #!/usr/bin/env node
-/*jslint stupid: true*/
 'use strict';
 
 var driver  = require('../lib/driver');
-var fs      = require('fs');
+var options = require('../lib/options');
 
-var options = {
-  hostname : 'localhost',
-  port     : 4444,
-  browsers : [{
-    name   : 'chrome'
-  }]
-};
-
-if (fs.existsSync('.min-wd')) {
-  var overrides = JSON.parse(fs.readFileSync('.min-wd').toString());
-  var k;
-  for (k in overrides) {
-    if (overrides.hasOwnProperty(k)) {
-      options[k] = overrides[k];
-    }
-  }
-}
-
-driver(process.stdin, options).pipe(process.stdout);
+driver(process.stdin, options()).pipe(process.stdout);
