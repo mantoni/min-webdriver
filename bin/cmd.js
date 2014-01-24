9906fca733e1a64b168467add880b693854f3a9f
Maximilian Antoni <mail@maxantoni.de>
2014-01-24 13:51:57 +0100

    Add callback to API

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,4 +4,10 @@
 var driver  = require('../lib/driver');
 var options = require('../lib/options');
 
-driver(process.stdin, options()).pipe(process.stdout);
+driver(process.stdin, options(), function (err) {
+  if (err) {
+    process.nextTick(function () {
+      process.exit(1);
+    });
+  }
+}).pipe(process.stdout);
